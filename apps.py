2c3a015988b021d8a7ce2a8f9b24931e32e5bdeb
saschakliegel <sascha.kliegel@web.de>
2021-03-18 14:33:12 +0800

    m

--- apps.py
+++ apps.py
@@ -38,14 +38,53 @@ if url != 'insert url here' or uploaded_text != 'Upload a PDF/Text here:':
         formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
         formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
+         sentence_list = nltk.sent_tokenize(article_text)
+
+
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+
+    maximum_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+
+
+    sentence_score = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_score.keys():
+                        sentence_score[sent] = word_frequencies[word]
+                    else:
+                        sentence_score[sent] += word_frequencies[word]
+
+
+    import heapq
+
+    sentences = st.slider('Please choose the number of sentences to display',min_value= 5,max_value= 30, value=10, step=1, key = 'First Slider')
+    summary_sentences = heapq.nlargest(sentences, sentence_score, key=sentence_score.get)
+
+    summary = ' '.join(summary_sentences)
+
+    st.write(summary)
+
     if uploaded_text:
         article_text = st.write(uploaded_text.read()), str()
 
         article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
         article_text = re.sub(r'\s+', ' ', article_text)
-
-
-
+        
         formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
         formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
