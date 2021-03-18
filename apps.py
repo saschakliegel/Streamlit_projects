dee58ba65d2c0db8a86ee37dba2aa169df4108ec
saschakliegel <sascha.kliegel@web.de>
2021-03-18 14:17:20 +0800

    m

--- apps.py
+++ apps.py
@@ -35,6 +35,13 @@ if url is not 'insert url here' or uploaded_text is not 'Upload a PDF/Text here:
     if uploaded_text:
         article_text = st.write(uploaded_text.read()), str()
 
+        article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+        article_text = re.sub(r'\s+', ' ', article_text)
+
+
+
+        formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+        formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
 
     article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
